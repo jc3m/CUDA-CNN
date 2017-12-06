fddd59eabbcfb9b3438a5cf863b418841e04fced
evanlissoos <evanlissoos@gmail.com>
2017-12-05 19:00:22 -0600

    JJ is a bitch 🔥💯😂

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -6,6 +6,24 @@
 #define FEATURE_NUM 64
 #define IMG_SIDE_LENGTH 24
 #define IMG_AREA (IMG_SIDE_LENGTH * IMG_SIDE_LENGTH)
+#define CUDA_MAX_NUM_THREADS 1024
+
+//Fast ceil macro that doesn't require float casting
+#define int_ceil(x,y) (x + y - 1) / y;
+
+//Network constants
+//B: 10000, M: 50, C: 1, H: 28, W: 28, K: 5
+#define M 50
+#define C 1
+#define H 28
+#define W 28
+#define K 5
+
+__constant__ float k_const[][][][];
+
+#define y4d(i3,i2,i1,i0) y[(i3) * (M * H_out * W_out) + (i2)*(H_out * W_out) + (i1)*(W_out) + i0]
+#define x4d(i3,i2,i1,i0) x[(i3) * (C * H * W) + (i2)*(H * W) + (i1)*(W) + i0]
+#define k4d(i3,i2,i1,i0) k[(i3) * (C * K * K) + (i2)*(K * K) + (i1)*(K) + i0]
 
 #include <mxnet/base.h>
 
@@ -21,7 +39,28 @@ namespace op
 \________\________|  |__/____  >  (____  /   |___  /__||__|  \___  >___|  /
 									\/        \/        \/              \/     \/
 */
-__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K) {
+__device__ void unroll_kernel(const int C, const int H, const int W, const int K, float *X, float *X_unroll, const int H_out, const int W_out) {
+	int t = blockIdx.x * CUDA_MAX_NUM_THREADS + threadIdx.x;
+	int W_unroll = H_out * W_out;
+
+	if(t < C * W_unroll) {
+		//It'd be cool to speed these up with some inline
+		int c = t / W_unroll;
+		int s = t % W_unroll;
+		int h_out = s / W_out;
+		int w_out = s % W_out;
+
+		int H_unroll = h_out * W_out + w_out;
+		int W_base = c * K * K;
+
+		for(int p = 0; p < K; p++) {
+			int w_unroll = w_base + p * K + q;
+			X_unroll[h_unroll, w_unroll] = X[c, h_out + p, w_out + q];
+		}
+	}
+}
+
+__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K, const int H_out, const int W_out) {
 
     /*
     Modify this function to implement the forward pass described in Chapter 16.
@@ -33,39 +72,14 @@ __global__ void forward_kernel(float *y, const float *x, const float *k, const i
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
 
-    // An example use of these macros:
-    // float a = y4d(0,0,0,0)
-    // y4d(0,0,0,0) = a
-    #define y4d(i3,i2,i1,i0) y[(i3) * (M * H_out * W_out) + (i2)*(H_out * W_out) + (i1)*(W_out) + i0]
-    #define x4d(i3,i2,i1,i0) x[(i3) * (C * H * W) + (i2)*(H * W) + (i1)*(W) + i0]
-    #define k4d(i3,i2,i1,i0) k[(i3) * (C * K * K) + (i2)*(K * K) + (i1)*(K) + i0]
 
-    /*
-        Your code here!
-    */
-    int img = blockIdx.x;										// b
-    int feature = blockIdx.y;									// m
-    unsigned int W_grid = (unsigned int)ceil((float)W_out / IMG_SIDE_LENGTH);
-    int height = blockIdx.z / W_grid + threadIdx.y;					// h
-    int width = blockIdx.z % W_grid + threadIdx.x;					// w
+}
 
-    if (height < H && width < W) {
-        float sum = 0.0f;
-        // Sum over all feature maps
-        for (int c = 0; c < C; ++c) {
-            // Single convolution step: KxK filter
-            for (int p = 0; p < K; ++p) {
-                for (int q = 0; q < K; ++q) {
-                    sum += x4d(img, c, height + p, width + q) * k4d(feature, c, p, q);
-                }
-            }
-        }
-        y4d(img, feature, height, width) = sum;
-    }
+static void unroll(const int C, const int H, const int W, const int K, float *X, float *X_unroll, const int H_out, const int W_out) {
+	unsigned int num_threads = C * H_out * W_out;
+	unsigned int num_blocks = int_ceil(num_threads, CUDA_MAX_NUM_THREADS);
 
-    #undef y4d
-    #undef x4d
-    #undef k4d
+	unroll_kernel<<<num_blocks, CUDA_MAX_NUM_THREADS>>>(C, H, W, K, X, X_unroll);
 }
 
 /*
@@ -94,8 +108,8 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
     // Set the kernel dimensions
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
-    unsigned int W_grid = (unsigned int)ceil((float)W_out / IMG_SIDE_LENGTH);
-    unsigned int H_grid = (unsigned int)ceil((float)H_out / IMG_SIDE_LENGTH);
+    unsigned int W_grid = int_ceil(W_out,IMG_SIDE_LENGTH);
+    unsigned int H_grid = int_ceil(H_out,IMG_SIDE_LENGTH);
     unsigned int Z = W_grid * H_grid;
     dim3 gridDim = {
         (unsigned int)B,
