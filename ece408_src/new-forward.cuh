a8abf05217366ca75ddf6abbcce292283a8f2a23
Jonathan Xu <jonathanj.xu97@gmail.com>
2017-12-07 12:45:50 -0600

    Clean up code

--- ece408_src/new-forward.cuh
+++ ece408_src/new-forward.cuh
@@ -15,30 +15,20 @@
 
 //Network constants
 //B: 10000, M: 50, C: 1, H: 28, W: 28, K: 5
-#define M 50
-#define C 1
-#define H 28
-#define W 28
-#define K 5
-
-#define numARows M
-#define numAColumns C * K * K
-#define numBRows C * K * K
-#define numBColumns H_out * W_out
-#define numCRows M
-#define numCColumns H_out * W_out
+#define M 50    // Number of the output feature maps
+#define C 1     // Number of input feature maps
+#define H 28    // Height of an input feature map
+#define W 28    // Width of an input feature map
+#define K 5     // Side length of a filter
 
 #define H_out (H - K + 1)
 #define W_out (W - K + 1)
-// #define W_unroll (C * K * K)
-// #define H_unroll (H_out * W_out)
 
 #define y4d(i3,i2,i1,i0) y[(i3) * (M * H_out * W_out) + (i2)*(H_out * W_out) + (i1)*(W_out) + i0]
 #define x4d(i3,i2,i1,i0) x[(i3) * (C * H * W) + (i2)*(H * W) + (i1)*(W) + i0]
 #define k4d(i3,i2,i1,i0) k[(i3) * (C * K * K) + (i2)*(K * K) + (i1)*(K) + i0]
 
 #include <mxnet/base.h>
-// #include "matrix_mul.cuh"
 
 namespace mxnet
 {
@@ -53,40 +43,51 @@ namespace op
                              \/        \/        \/              \/     \/
 */
 __global__ void matrixMultiplyShared(float *A, float *B, float *Carr) {
-  //@@ Insert code to implement matrix multiplication here
-  //@@ You have to use shared memory for this MP
-  //@@ Insert code to implement matrix multiplication here
-  __shared__ float subTileA[TILE_WIDTH][TILE_WIDTH];
-  __shared__ float subTileB[TILE_WIDTH][TILE_WIDTH];
+    #define numARows M
+    #define numAColumns C * K * K
+    #define numBRows C * K * K
+    #define numBColumns H_out * W_out
+    #define numCRows M
+    #define numCColumns H_out * W_out
+    __shared__ float subTileA[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float subTileB[TILE_WIDTH][TILE_WIDTH];
 
-  int Row = blockIdx.y * TILE_WIDTH + threadIdx.y;
-  int Col = blockIdx.x * TILE_WIDTH + threadIdx.x;
-  float Pvalue = 0;
+    int Row = blockIdx.y * TILE_WIDTH + threadIdx.y;
+    int Col = blockIdx.x * TILE_WIDTH + threadIdx.x;
+    float Pvalue = 0;
 
-  for (int m = 0; m < ceil(numAColumns/(float)TILE_WIDTH); m++) {
-    // Parallel memory reads
-    if((Row < numCRows) && (m * TILE_WIDTH + threadIdx.x < numAColumns)) {
-      subTileA[threadIdx.y][threadIdx.x] = A[Row*numAColumns + m*TILE_WIDTH + threadIdx.x];
-    } else {
-      subTileA[threadIdx.y][threadIdx.x] = 0;
+    for (int m = 0; m < ceil(numAColumns/(float)TILE_WIDTH); m++) {
+        // Parallel memory reads
+        if ((Row < numCRows) && (m * TILE_WIDTH + threadIdx.x < numAColumns)) {
+            subTileA[threadIdx.y][threadIdx.x] = A[Row*numAColumns + m*TILE_WIDTH + threadIdx.x];
+        } else {
+            subTileA[threadIdx.y][threadIdx.x] = 0;
+        }
+
+        if ((m * TILE_WIDTH + threadIdx.y < numBRows) && (Col < numCColumns)) {
+            subTileB[threadIdx.y][threadIdx.x] = B[(m*TILE_WIDTH + threadIdx.y)*numBColumns + Col];
+        } else {
+            subTileB[threadIdx.y][threadIdx.x] = 0;
+        }
+
+        __syncthreads();
+
+        // Tile calculation
+        for (int k = 0; k < TILE_WIDTH; k++) {
+            Pvalue += subTileA[threadIdx.y][k] * subTileB[k][threadIdx.x];
+        }
+
+        __syncthreads();
     }
-
-    if((m * TILE_WIDTH + threadIdx.y < numBRows) && (Col < numCColumns)) {
-      subTileB[threadIdx.y][threadIdx.x] = B[(m*TILE_WIDTH + threadIdx.y)*numBColumns + Col];
-    } else {
-      subTileB[threadIdx.y][threadIdx.x] = 0;
+    if ((Row < numCRows) && (Col < numCColumns)) {
+        Carr[Row * numCColumns + Col] = Pvalue;
     }
-
-    __syncthreads();
-
-    // Tile calculation
-    for (int k = 0; k < TILE_WIDTH; k++)
-      Pvalue += subTileA[threadIdx.y][k] * subTileB[k][threadIdx.x];
-
-    __syncthreads();
-  }
-  if((Row < numCRows) && (Col < numCColumns))
-    Carr[Row * numCColumns + Col] = Pvalue;
+    #undef numARows
+    #undef numAColumns
+    #undef numBRows
+    #undef numBColumns
+    #undef numCRows
+    #undef numCColumns
 }
 
 __global__ void unroll_kernel(float *x, float *X_unroll) {
@@ -95,13 +96,9 @@ __global__ void unroll_kernel(float *x, float *X_unroll) {
     int t = blockIdx.x * CUDA_MAX_NUM_THREADS + threadIdx.x;
 
     int W_unroll = H_out * W_out;           // Number of elements in an output feature map
-    // int threadsPerImage = C * W_unroll;     // C * H_out * W_out
-    // int totalThreads = B * threadsPerImage; // B * C * H_out * W_out
 
     if (t < W_unroll) {
         // It'd be cool to speed these up with some inline
-        // int b = t / threadsPerImage;            // The image # in the batch
-        // int i = t % threadsPerImage;            // InputFeatureMap/OutputElement combo in the given image
         int c = t / W_unroll;                   // The given feature map
         int s = t % W_unroll;                   // Linearized index in the output feature map
         int h_out = s / W_out;                  // Height in the output feature map
@@ -124,15 +121,6 @@ __global__ void unroll_kernel(float *x, float *X_unroll) {
     }
 }
 
-__global__ void forward_kernel(float *y, const float *x, const float *k, const int B) {
-    /*
-    Modify this function to implement the forward pass described in Chapter 16.
-    We have added an additional dimension to the tensors to support an entire mini-batch
-    The goal here is to be correct AND fast.
-    We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
-    */
-}
-
 static void unroll(float *X, float *X_unroll, cudaStream_t s) {
     unsigned int num_threads = C * H_out * W_out;
     unsigned int num_blocks = int_ceil(num_threads, CUDA_MAX_NUM_THREADS);
@@ -153,20 +141,10 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
     // Extract the tensor dimensions into B,M,C,H,W,K
     const int B = x.shape_[0]; // Number of Images, y.shape_[0] should be the same
-    // const int M = y.shape_[1]; // Number of the output feature maps
-    // const int C = x.shape_[1]; // Number of input feature maps
-    // const int H = x.shape_[2]; // Height of an input feature map
-    // const int W = x.shape_[3]; // Width of an input feature map
-    // const int K = w.shape_[3]; // Side length of a filter
-
-    // H_out and W_out should both be 24
 
     // Unroll
     float *X_unroll;
     cudaMalloc(&X_unroll, sizeof(float) * C * K * K * H_out * W_out);
-
-    // Call the kernel
-    // forward_kernel<<<gridDim, blockDim, 0, s>>>(y.dptr_,x.dptr_,w.dptr_, B,M,C,H,W,K);
 
     for (int b = 0; b < B; b++) {
         float *xb = &(x.dptr_[b * C * H * W]);
@@ -175,10 +153,10 @@ void forward<gpu, float>(mshadow::Tensor<gpu, 4, float> &y, const mshadow::Tenso
 
         float *yb = &(y.dptr_[b * M * H_out * W_out]);
 
-        int k_rows = M;
-        int k_cols = C * K * K;
-        int x_rows = C * K * K;
-        int x_cols = H_out * W_out;
+        // int k_rows = M;
+        // int k_cols = C * K * K;
+        // int x_rows = C * K * K;
+        // int x_cols = H_out * W_out;
         int y_rows = M;
         int y_cols = H_out * W_out;
 
